59677024df30078f3b986039ad31f99f7f9456b5
andy-b23 <andy-b23@gmx.at>
2017-11-28 22:35:12 -0600

    createPolygonFertig

--- src/Input.java
+++ src/Input.java
@@ -17,7 +17,7 @@ public class Input {
     private JButton btn_AddPoint;
     private JTextField txt_addPointX;
     private JTextField txt_addPointY;
-    private JButton btn_FinishPolygon;
+    private JButton btn_ResetPolygon;
     private JButton btn_ChoosePolygon;
     private JComboBox cmb_Polygon;
     private JTextField txt_tx;
@@ -35,13 +35,16 @@ public class Input {
     private JTextField txt_scalingFijoPuntoY;
     private JTextField txt_reflectionPuntoY;
     private JPanel main;
+    private JTextArea txtA_allPoints;
+    private JTextArea textArea1;
+    private JButton btn_transform;
 
     private Polygon polygon;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("InputValues");
         frame.setContentPane(new Input().main);
-        frame.setPreferredSize(new Dimension(600, 400));
+        frame.setPreferredSize(new Dimension(800, 400));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -49,17 +52,102 @@ public class Input {
 
     public Input() {
 
+        polygon = new Polygon();
+
         btn_AddPoint.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     int x = Integer.parseInt(txt_addPointX.getText());
                     int y = Integer.parseInt(txt_addPointY.getText());
-                }catch (NumberFormatException e){
-                    JOptionPane.showMessageDialog(null,"Error Not a Number");
+
+                    txt_addPointX.setText("");
+                    txt_addPointY.setText("");
+
+                    polygon.addPoint(x, y);
+
+                    txtA_allPoints.append(polygon.npoints + " x: " + x + " y: " + y + "\n");
+
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Error Not a Number");
                 }
             }
         });
 
+        btn_ChoosePolygon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                polygon.reset();
+                clearFields();
+
+                String choosenPolygon = cmb_Polygon.getSelectedItem().toString();
+                switch (choosenPolygon) {
+                    case "Rectangle":
+                        polygon.addPoint(5, 5);
+                        txtA_allPoints.append(polygon.npoints + " x: 5  y: 5\n");
+
+                        polygon.addPoint(5, 10);
+                        txtA_allPoints.append(polygon.npoints + " x: 5  y: 10\n");
+
+                        polygon.addPoint(10, 5);
+                        txtA_allPoints.append(polygon.npoints + " x: 10  y: 5\n");
+
+                        polygon.addPoint(10, 10);
+                        txtA_allPoints.append(polygon.npoints + " x: 10  y: 10\n");
+
+                        break;
+                    case "Triangle":
+                        polygon.addPoint(5, 5);
+                        txtA_allPoints.append(polygon.npoints + " x: 5  y: 5\n");
+
+                        polygon.addPoint(7, 10);
+                        txtA_allPoints.append(polygon.npoints + " x: 7  y: 10\n");
+
+                        polygon.addPoint(12, 6);
+                        txtA_allPoints.append(polygon.npoints + " x: 12  y: 6\n");
+                        break;
+                    case "Five-sided":
+                        polygon.addPoint(5, 5);
+                        txtA_allPoints.append(polygon.npoints + " x: 5  y: 5\n");
+
+                        polygon.addPoint(3, 10);
+                        txtA_allPoints.append(polygon.npoints + " x: 3  y: 10\n");
+
+                        polygon.addPoint(7, 11);
+                        txtA_allPoints.append(polygon.npoints + " x: 7  y: 11\n");
+
+                        polygon.addPoint(10, 2);
+                        txtA_allPoints.append(polygon.npoints + " x: 10  y: 2\n");
+
+                        polygon.addPoint(7, 7);
+                        txtA_allPoints.append(polygon.npoints + " x: 7  y: 7\n");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        btn_ResetPolygon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                polygon.reset();
+                clearFields();
+            }
+        });
+    }
+
+    private void clearFields() {
+        txtA_allPoints.setText("");
+        txt_addPointX.setText("");
+        txt_addPointY.setText("");
+    }
+
+    public Polygon getPolygon() {
+        return polygon;
+    }
+
+    public void setPolygon(Polygon polygon) {
+        this.polygon = polygon;
     }
 }
