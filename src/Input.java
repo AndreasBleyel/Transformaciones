2fce6e2b50b045b55b935177fab304120a96acb7
andy-b23 <andy-b23@gmx.at>
2017-11-29 13:20:39 -0600

    rotationOKstartScaling

--- src/Input.java
+++ src/Input.java
@@ -23,8 +23,8 @@ public class Input {
     private JTextField txt_tx;
     private JTextField txt_ty;
     private JTextField txt_gradRotationOrigin;
-    private JTextField txt_pivoteRotationPivot;
-    private JTextField txt_gradRotationPivot;
+    private JTextField txt_rotationPivotY;
+    private JTextField txt_rotationPivotX;
     private JTextField txt_scalingFijoSx;
     private JTextField txt_scalingFijoSy;
     private JTextField txt_scalingFijoPuntoX;
@@ -41,6 +41,12 @@ public class Input {
     private JLabel lbl_tx;
     private JLabel lbl_ty;
     private JLabel lbl_gradRotationOrigin;
+    private JLabel lbl_rotationPivotX;
+    private JLabel lbl_rotationPivotY;
+    private JLabel lbl_scalFijoSX;
+    private JLabel lbl_scalFijoSY;
+    private JLabel lbl_scalFijoPuntoX;
+    private JLabel lbl_scalFijoPuntoY;
 
     private Polygon polygonStart;
     private Polygon polygonEnd;
@@ -149,16 +155,33 @@ public class Input {
                         }
                         break;
                     case "rp":
+                        try {
+                            double grad = Math.toRadians(Double.parseDouble(txt_gradRotationOrigin.getText()));
+                            int pivotX = Integer.parseInt(txt_rotationPivotX.getText());
+                            int pivotY = Integer.parseInt(txt_rotationPivotY.getText());
+                            rotacionPivote(pivotX, pivotY, grad);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Error Not a Number");
+                        }
                         break;
                     case "ro":
                         try {
-                            int grad = Integer.parseInt(txt_gradRotationOrigin.getText());
+                            double grad = Math.toRadians(Double.parseDouble(txt_gradRotationOrigin.getText()));
                             rotacionOrigen(grad);
                         } catch (NumberFormatException e) {
                             JOptionPane.showMessageDialog(null, "Error Not a Number");
                         }
                         break;
                     case "sp":
+                        try {
+                            double sx = Double.parseDouble(txt_scalingFijoSx.getText());
+                            double sy = Double.parseDouble(txt_scalingFijoSy.getText());
+                            int pivotX = Integer.parseInt(txt_scalingFijoPuntoX.getText());
+                            int pivotY = Integer.parseInt(txt_scalingFijoPuntoY.getText());
+                            scalingPuntoFijo(sx, sy, pivotX, pivotY);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Error Not a Number");
+                        }
                         break;
                     case "sg":
                         break;
@@ -184,6 +207,7 @@ public class Input {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 typeOfTransformation = "t";
+                setAllInvisible();
                 txt_tx.setVisible(true);
                 txt_ty.setVisible(true);
                 lbl_tx.setVisible(true);
@@ -196,9 +220,45 @@ public class Input {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 typeOfTransformation = "ro";
+                setAllInvisible();
+
                 txt_gradRotationOrigin.setVisible(true);
                 lbl_gradRotationOrigin.setVisible(true);
                 btn_transform.setText("Rotation Origin");
+                btn_transform.setEnabled(true);
+            }
+        });
+        btn_RotationPivot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                typeOfTransformation = "rp";
+                setAllInvisible();
+
+                txt_gradRotationOrigin.setVisible(true);
+                lbl_gradRotationOrigin.setVisible(true);
+                txt_rotationPivotX.setVisible(true);
+                lbl_rotationPivotX.setVisible(true);
+                txt_rotationPivotY.setVisible(true);
+                lbl_rotationPivotY.setVisible(true);
+                btn_transform.setText("Rotation Pivot");
+                btn_transform.setEnabled(true);
+            }
+        });
+        btn_ScalingFijo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                typeOfTransformation = "sp";
+                setAllInvisible();
+
+                txt_scalingFijoPuntoX.setVisible(true);
+                txt_scalingFijoPuntoY.setVisible(true);
+                lbl_scalFijoSX.setVisible(true);
+                lbl_scalFijoSY.setVisible(true);
+                txt_scalingFijoSx.setVisible(true);
+                txt_scalingFijoSy.setVisible(true);
+                lbl_scalFijoPuntoX.setVisible(true);
+                lbl_scalFijoPuntoY.setVisible(true);
+                btn_transform.setText("Scaling Pivot");
                 btn_transform.setEnabled(true);
             }
         });
@@ -215,60 +275,75 @@ public class Input {
         fillTextArea(txtA_endPoints, polygonEnd);
     }
 
-    private void rotacionOrigen(int grad) {
+    private void rotacionOrigen(double grad) {
 
         double[][] rotationMatrix = new double[][]{
-                { Math.cos(grad), Math.sin(grad)*-1 },
-                { Math.sin(grad), Math.cos(grad) }
+                {Math.cos(grad), Math.sin(grad) * -1},
+                {Math.sin(grad), Math.cos(grad)}
         };
 
-
-        System.out.println("Rotation: \n-------");
-        System.out.println(rotationMatrix[0][0]+" "+rotationMatrix[0][1]+"\n"+rotationMatrix[1][0]+" "+rotationMatrix[1][1]);
+        /*System.out.println("Rotation: \n-------");
+        System.out.println(rotationMatrix[0][0] + " " + rotationMatrix[0][1] + "\n" + rotationMatrix[1][0] + " " + rotationMatrix[1][1]);
         System.out.println("\n--------");
-        double[][] pointToRotate = new double[1][2];
+*/
+        double[][] pointToRotate = new double[2][1];
         for (int i = 0; i < polygonStart.npoints; i++) {
-            pointToRotate[0][0]=polygonStart.xpoints[i];
-            pointToRotate[0][1]=polygonStart.ypoints[i];
-            double[][] result = multiplyByMatrix(pointToRotate, rotationMatrix);
-            polygonEnd.addPoint((int)Math.round(result[0][0]),(int)Math.round(result[0][1]));
+            pointToRotate[0][0] = polygonStart.xpoints[i];
+            pointToRotate[1][0] = polygonStart.ypoints[i];
+
+            double x = pointToRotate[0][0] * rotationMatrix[0][0] + pointToRotate[1][0] * rotationMatrix[0][1];
+            double y = pointToRotate[1][0] * rotationMatrix[0][0] + pointToRotate[1][0] * rotationMatrix[0][0];
+
+            polygonEnd.addPoint((int) Math.round(x), (int) Math.round(y));
         }
 
         fillTextArea(txtA_endPoints, polygonEnd);
 
     }
 
-    private void rotacionPivote() {
+    private void rotacionPivote(int pivotX, int pivotY, double grad) {
+
+        for (int i = 0; i < polygonStart.npoints; i++) {
+            int x = pivotX + (int) Math.round((polygonStart.xpoints[i] - pivotX) * Math.cos(grad) - (polygonStart.ypoints[i] - pivotY) * Math.sin(grad));
+            int y = pivotY + (int) Math.round((polygonStart.xpoints[i] - pivotX) * Math.sin(grad) + (polygonStart.ypoints[i] - pivotY) * Math.cos(grad));
+            //System.out.println(pivotX + "+("+polygonStart.xpoints[i] +"-"+pivotX+")*"+Math.cos(grad)+"+("+polygonStart.ypoints[i]+"-"+pivotY+")*"+Math.sin(grad));
+            polygonEnd.addPoint(x, y);
+        }
+
+        fillTextArea(txtA_endPoints, polygonEnd);
+    }
+
+    private void scalingPuntoFijo(double sx, double sy, int x, int y) {
 
     }
 
     private double[][] multiplyByMatrix(double[][] pointToRotate, double[][] rotationMatrix) {
 
-        int m1ColLength = pointToRotate[0].length; // m1 columns length
-        int m2RowLength = rotationMatrix.length;    // m2 rows length
+        int pointToRotateColLength = pointToRotate[0].length; // m1 columns length
+        int rotationMatrixLength = rotationMatrix.length;    // m2 rows length
 
-        if (m1ColLength != m2RowLength) return null; // matrix multiplication is not possible
+        if (pointToRotateColLength != rotationMatrixLength) {
+            JOptionPane.showMessageDialog(null, "Error matrix multiplication is not possible");
+            return null; // matrix multiplication is not possible
+        }
 
-        int mRRowLength = pointToRotate.length;    // m result rows length
-        int mRColLength = rotationMatrix[0].length; // m result columns length
+        int pointToRotateLength = pointToRotate.length;    // m result rows length
+        int rotationMatrixColLength = rotationMatrix[0].length; // m result columns length
 
-        double[][] mResult = new double[mRRowLength][mRColLength];
+        double[][] mResult = new double[pointToRotateLength][rotationMatrixColLength];
 
-        for (int i = 0; i < mRRowLength; i++) {         // rows from m1
-            for (int j = 0; j < mRColLength; j++) {     // columns from m2
-                for (int k = 0; k < m1ColLength; k++) { // columns from m1
+        for (int i = 0; i < pointToRotateLength; i++) {         // rows from m1
+            for (int j = 0; j < rotationMatrixColLength; j++) {     // columns from m2
+                for (int k = 0; k < pointToRotateColLength; k++) { // columns from m1
                     mResult[i][j] += pointToRotate[i][k] * rotationMatrix[k][j];
                 }
             }
         }
 
-
-        System.out.println(outputMatrix(mResult));
-
         return mResult;
     }
 
-    private String outputMatrix(double matrix[][]){
+    private String outputMatrix(double matrix[][]) {
         String result = "";
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -278,6 +353,7 @@ public class Input {
         }
         return result;
     }
+
     private void clearFields() {
         txtA_startingPoints.setText("");
         txtA_endPoints.setText("");
@@ -295,5 +371,21 @@ public class Input {
     private void setAllInvisible() {
         txt_tx.setVisible(false);
         txt_ty.setVisible(false);
+        lbl_tx.setVisible(false);
+        lbl_ty.setVisible(false);
+        txt_gradRotationOrigin.setVisible(false);
+        lbl_gradRotationOrigin.setVisible(false);
+        txt_rotationPivotX.setVisible(false);
+        lbl_rotationPivotX.setVisible(false);
+        txt_rotationPivotY.setVisible(false);
+        lbl_rotationPivotY.setVisible(false);
+        txt_scalingFijoPuntoX.setVisible(false);
+        txt_scalingFijoPuntoY.setVisible(false);
+        lbl_scalFijoSX.setVisible(false);
+        lbl_scalFijoSY.setVisible(false);
+        txt_scalingFijoSx.setVisible(false);
+        txt_scalingFijoSy.setVisible(false);
+        lbl_scalFijoPuntoX.setVisible(false);
+        lbl_scalFijoPuntoY.setVisible(false);
     }
 }
